897578c3de76688cff5f4d03c67a67e9f9b002bd
codingWithSharon <sharon_dt@hotmail.com>
2026-05-27 15:51:59 +0200

    Rearrangement of folders and files

---
+++ TA_Assignment_spriteCloud_Sharon_20260527/APITest/Helpers/SpecificHelpers/DummyJson/DummyJsonResponseHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TA_Assignment_spriteCloud_Sharon_20260527.APITest.Helpers.SpecificHelpers.DummyJson
+{
+    public class DummyJsonResponseHelper
+    {
+        private readonly IAPIRequestContext _context;
+
+        public DummyJsonResponseHelper(IAPIRequestContext context)
+        {
+            _context = context;
+        }
+
+        // GET request methods
+        //public async Task<IAPIResponse> PostAsyncJson(string endpoint, object? data = null, Dictionary<string, string>? headers = null)
+        //{
+        //    var requestOptions = new APIRequestContextOptions
+        //    {
+        //        DataObject = data,
+        //        Headers = headers
+        //    };
+
+        //    return await _context.PostAsync($"{AutomationExerciseBaseUrl}{endpoint}", requestOptions);
+        //}
+    }
+}
---
+++ TA_Assignment_spriteCloud_Sharon_20260527/APITest/Models/Responses/DummyJsonResponseModels.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TA_Assignment_spriteCloud_Sharon_20260527.APITest.Models.Responses
+{
+    public class LoginResponse
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string gender { get; set; }
+        public string image { get; set; }
+    }
+
+    public class ProductResponse
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public decimal DiscountPercentage { get; set; }
+        public decimal Rating { get; set; }
+        public int Stock { get; set; }
+        public List<string> Tags { get; set; }
+        public string Brand { get; set; }
+        public string Sku { get; set; }
+        public int Weight { get; set; }
+        public Dimensions Dimensions { get; set; }
+        public string WarrantyInformation { get; set; }
+        public string ShippingInformation { get; set; }
+        public string AvailabilityStatus { get; set; }
+        public List<Review> Reviews { get; set; }
+        public string ReturnPolicy { get; set; }
+        public int MinimumOrderQuantity { get; set; }
+        public Meta Meta { get; set; }
+        public List<string> Images { get; set; }
+        public string Thumbnail { get; set; }
+    }
+
+    public class Dimensions
+    {
+        public decimal Width { get; set; }
+        public decimal Height { get; set; }
+        public decimal Depth { get; set; }
+    }
+
+    public class Review
+    {
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public DateTime Date { get; set; }
+        public string ReviewerName { get; set; }
+        public string ReviewerEmail { get; set; }
+    }
+
+    public class Meta
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string Barcode { get; set; }
+        public string QrCode { get; set; }
+    }
+}
+
