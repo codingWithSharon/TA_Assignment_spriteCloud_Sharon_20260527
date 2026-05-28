b08f0a76d0ef11e01f8c9faa662b80c1a59ebf5c
codingWithSharon <sharon_dt@hotmail.com>
2026-05-28 22:44:47 +0200

    Added FullCheckout test for SauceDemo

--- TA_Assignment_spriteCloud_Sharon_20260527/UITest/PageObjectModels/BasePage.cs
+++ TA_Assignment_spriteCloud_Sharon_20260527/UITest/PageObjectModels/BasePage.cs
@@ -11,4 +11,23 @@ public abstract class BasePage : ContextTest
     {
         Page = page;
     }
+
+    public readonly string SauceDemoBaseUrl = Environment.GetEnvironmentVariable("SauceDemoBaseUrl");
+
+    #region SauceDemo specific locators
+    public ILocator _pageTitle => Page.Locator("[data-test='title']");
+    #endregion
+
+    public async Task PageLauncher(string baseUrl, string pageExtension = "")
+    {
+        var fullUrl = new Uri(new Uri(baseUrl), pageExtension).ToString();
+
+        await Page.GotoAsync(fullUrl, new PageGotoOptions
+        {
+            WaitUntil = WaitUntilState.NetworkIdle,
+            Timeout = 30_000
+        });
+
+        await Page.WaitForLoadStateAsync(LoadState.DOMContentLoaded);
+    }
 }
---
+++ TA_Assignment_spriteCloud_Sharon_20260527/UITest/PageObjectModels/SauceDemoPages/SauceDemoCartPage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TA_Assignment_spriteCloud_Sharon_20260527.UITests.PageObjectModels;
+
+namespace TA_Assignment_spriteCloud_Sharon_20260527.UITest.PageObjectModels.SauceDemoPages
+{
+    public class SauceDemoCartPage : BasePage
+    {
+        public SauceDemoCartPage(IPage page) : base(page) { }
+
+        #region general
+        public ILocator GetPriceByItemName(string itemName) => Page.Locator("[data-test='inventory-item']")
+        .Filter(new() { HasText = itemName })
+        .Locator("[data-test='inventory-item-price']");
+        public ILocator GetQuantityByItemName(string itemName) => Page.Locator("[data-test='inventory-item']")
+        .Filter(new() { HasText = itemName })
+        .Locator("[data-test='item-quantity']");
+        public ILocator _CheckoutButton => Page.Locator("#checkout");
+        #endregion
+
+        #region basic operations login page
+        public async Task ClickCheckoutButton() => await _CheckoutButton.ClickAsync();
+        #endregion
+    }
+}
